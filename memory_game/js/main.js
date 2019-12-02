cfd1f54707db685fe2ec1cbaa7f9a856cc91478f
rachellesg <rachelle@Rachelles-MacBook-Pro.local>
2019-12-02 20:33:31 +0800

    added more game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png",
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png",
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds/png"
+}
+];
 
 const cardsInPlay = [];
 
@@ -11,8 +32,10 @@ function checkForMatch() {
 }
 
 function flipCard(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cardId);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log("User flipped " + cards[cardId].suit);
+	console.log("User flipped " + cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
 
 	checkForMatch();
 }
