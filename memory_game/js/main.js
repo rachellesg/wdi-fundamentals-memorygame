f70a9a20a3866e73c55293796d400f0f827612ec
rachellesg <rachelle@Rachelles-MacBook-Pro.local>
2019-12-02 23:14:14 +0800

    updated more js code

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -31,14 +31,27 @@ function checkForMatch() {
 	}
 }
 
-function flipCard(cardId) {
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+
+	if (cardsInPlay.length === 2 ) {
+		checkForMatch();
+	}
 	console.log("User flipped " + cards[cardId].rank);
 	console.log("User flipped " + cards[cardId].suit);
 	console.log("User flipped " + cards[cardId].cardImage);
-	cardsInPlay.push(cards[cardId].rank);
-
-	checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
+function createBoard () {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+createBoard();
